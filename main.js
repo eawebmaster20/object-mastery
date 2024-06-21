98c848b74ca7ca2e0c140550d73d39d508588f2c
eawebmaster20 <eaweb20@gmail.com>
2024-06-21 08:05:43 +0000

    feat: completed the code for the task 5

--- main.js
+++ main.js
@@ -27,3 +27,33 @@ SuperHero.prototype.revealIdentity = function(){console.log(`hero's secret ident
 const heroOne= new SuperHero('Benjamin', 'fire', ['fire','fuel'])
 heroOne.usePower('dk');
 heroOne.revealIdentity();
+
+
+/************************************************************************
+ * TASK 5 & 6
+ ***********************************************************************/
+
+let characters=[ {  name: 'Mr. Nice', secretIdentity:'hero', powers:['magnet'],weakness:'' },
+  { name: 'Narco', secretIdentity:'hero', powers:['vision'],weakness:'' },
+  { name: 'Bombasto', secretIdentity:'villian', powers:['happiness'],weakness:'' },
+  { name: 'Celeritas', secretIdentity:'hero', powers:['wickedness'],weakness:'' },
+  { name: 'Magneta', secretIdentity:'villian', powers:['murder'],weakness:'' },
+  { name: 'RubberMan', secretIdentity:'villian', powers:['anger'],weakness:'' },
+  { name: 'Dynama', secretIdentity:'villian', powers:['rigidness'],weakness:'' },
+  { name: 'Dr IQ', secretIdentity:'hero', powers:['comfort'],weakness:'' },
+  { name: 'Magma', secretIdentity:'villian', powers:['disguise'],weakness: ''},
+  { name: 'Tornado', secretIdentity:'hero', powers:['expert chemist'],weakness:'intellect' }]
+// for each
+characters.forEach(el=>{
+    if (el.secretIdentity === 'hero') {
+        el.weakness = 'passion'
+    }
+})
+console.log(characters);
+
+// map
+characters.map(char=>char.weakness =char.powers[0])
+console.log(characters);
+
+// filter
+console.log(characters.filter(char=> char.secretIdentity === 'villian'));
