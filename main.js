68521b0985b6605fc88b847f3d7bac70abfe99f6
eawebmaster20 <eaweb20@gmail.com>
2024-06-21 08:03:01 +0000

    feat: completed the code for the task 1 and 2

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 /******************************************************************
  * TASK 1 & 2
  *****************************************************************/
+let superHeroObject = {
+    name:'Benjamin',
+    secretIdentity: 'fire',
+    powers:['fire','wind'],
+    weekness:'water',
+    usePower(powerName){console.log(`hero ${this.name} is using the ${powerName} power`)},
+    revealIdentity(){console.log(`hero's secret identity is ${this.secretIdentity}`)}
+}
+superHeroObject.usePower('flying');
+superHeroObject.revealIdentity();
